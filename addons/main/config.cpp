3beacd27798c418e551ae2cb0dba7fe1af113758
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-01-25 22:17:31 -0500

    feat: add main addon

---
+++ addons/main/config.cpp
@@ -0,0 +1,14 @@
+class CfgPatches {
+    class fdelta_stats_main {
+        name = "Stats Tracker";
+        author = "thegamecracks";
+        url = "https://github.com/thegamecracks/420th-Stats-Tracker";
+
+        requiredVersion = 2.20;
+        requiredAddons[] = {
+        };
+        skipWhenMissingDependencies = 0;
+
+        units[] = {};
+    };
+};
